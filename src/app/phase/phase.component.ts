ebf5452a5ab8fe61dbf8ce555103633e1f579de1
Rewas Safwat <rewasssafwat51@gmail.com>
2023-11-06 00:10:20 +0200

    edit some variables

--- src/app/phase/phase.component.ts
+++ src/app/phase/phase.component.ts
@@ -12,65 +12,57 @@ import { CardInterface as Card } from '../interfaces/card-interface';
   styleUrls: ['./phase.component.css']
 })
 export class PhaseComponent {
-
+/* get phase and all phases from parent commponent (Board Compenent) */
   @Input() phase! : any|Phase;
   @Input() phases : Array<Phase>= [];
 
+
   @Output() unSave = new EventEmitter<boolean>();
   @Output() cards = new EventEmitter<Array<Card>>();
+
+/* send data to paprent to delete phase from board */
   @Output() delPhase = new EventEmitter<number>();
   
   isUnSave: boolean = false;
   allMoves: Array<Card> = [];
-  newcard: string = '';
+
+/* all variables for phases edit or delete or update and */
+  isThreeDotsVisible: boolean = false;
   editIndex: number = -1;
   showInput: boolean = false;
-  editCardTitle: boolean = false;
   editPhaseTitle: boolean = false;
-  isThreeDotsVisible: boolean = false;
 
+/* all variables for cards on phases edit title and delete and update and add */
+  isCardOpen:boolean=true;
+  card!:Card;
+  editCardTitle: boolean = false;
+  newcard: string = '';
+
+/* initialize all variables to use services connect with database for any query */
   constructor(private phaseService: PhaseService,private cardService: CardService){};
   
-  ngOnInit(){
-    this.phaseService.getAllPhases().subscribe(
-      (data:any) => {
-        this.phases = data.data;
-      },
-      (error)=>  console.log(error),
-    );
+/* ---------------------- all crud functions on phase -------------------- */
 
-    }
-  
+/* toggle btn option delete phase */
+toggleThreeDots(){
+  this.isThreeDotsVisible = !this.isThreeDotsVisible;
+}
 
-    toggleThreeDots(){
-      this.isThreeDotsVisible = !this.isThreeDotsVisible;
-    }
-
-    /* phase functions*/
-    deletePhase(id:number){
+/* delete phase by send to parent and delete and remove this phase from front */
+  deletePhase(id:number){
     this.isThreeDotsVisible = false;
-     this.delPhase.emit(id);
-     this.phase = null;
-    }
-
-
-
-    isCardOpen:boolean=true;
-    card!:Card;
-
-    openCard(card:Card){
-     this.isCardOpen=false;
-     this.card=card;
-    }
-    
-    
-
-
+    this.delPhase.emit(id);
+    this.phase = null;
+  }
+  
+  
+  /*----------------- function open edit title for phase  --------------*/
   enableTitleEdit() {
     
     this.editPhaseTitle = true;
   }
-
+  
+  /*----------------- function when click enter close edit title for phase and savev change update on phase  --------------*/
   disableTitleEdit() {
     if (this.phase.title) {
       let phase = {
@@ -81,18 +73,37 @@ export class PhaseComponent {
       this.phaseService.updatePhase(phase,this.phase.id).subscribe(
         (res:any) => (this.phase = res.data),
         (error)=>  console.log(error),
-        );
+      );
     }
     this.editPhaseTitle = false;
-
+      
   }
+/* ---------------- end crud functions on phases ----------------- */
+    
+    /* ----------------------------------------------- */
+    
+/* ---------------- all crud functions on cards ----------------- */
+    
+/* function open details in card as popup */
+  openCard(card:Card){
+    this.isCardOpen=false;
+    this.card=card;
+  }
+   
 
+/* function open add new card by button add card */
   addcard() {
     this.showInput = true;
     this.newcard = ''; 
   }
 
-
+/* function cancel add card */
+  cancelcard() {
+    this.newcard = '';
+    this.showInput = false;
+  }
+  
+/* function add and save on database card */
   saveCard() {
     if (this.newcard) {
       let card = {
@@ -111,16 +122,14 @@ export class PhaseComponent {
       
   }
 
-  cancelcard() {
-    this.newcard = '';
-    this.showInput = false;
-  }
 
+/* function open button update title cards */ 
   enableCardTitleEdit(index: number) {
     this.newcard = this.phase.cards![index].title;
     this.editIndex = index;
   }
 
+/* function update card title */ 
   updateCardTitle(index:number){
     if (this.newcard) {
       let card = {
@@ -138,7 +147,7 @@ export class PhaseComponent {
   }
 
   
-  
+/* function on exchange cards (drag and drop) */
  async onDrop(event: CdkDragDrop<any>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
@@ -162,21 +171,23 @@ export class PhaseComponent {
         this.unSave.emit(true);
         
     } 
-      
-    rankingCardCurrent(cards: Array<Card>, phase_id: number|null, event: CdkDragDrop<any>) {
-      cards.forEach( element => {
-        const index = cards.indexOf(element);
-        element.position = index;
-        element.phase_id = phase_id?phase_id:element.phase_id;
-        if(this.allMoves.length>0){
-          let indexMove = this.allMoves.indexOf(element);
-          this.allMoves.includes(element)?this.allMoves[indexMove]=element:this.allMoves.push(element);
-        }else {
-          this.allMoves.push(element);
-        }
-      });
-      this.cards.emit(this.allMoves); 
-    }
+    
+/* update positions for cards after change drag and drop poistions cards ui */
+rankingCardCurrent(cards: Array<Card>, phase_id: number|null, event: CdkDragDrop<any>) {
+  cards.forEach( element => {
+    const index = cards.indexOf(element);
+      element.position = index;
+      element.phase_id = phase_id?phase_id:element.phase_id;
+      if(this.allMoves.length>0){
+        let indexMove = this.allMoves.indexOf(element);
+        this.allMoves.includes(element)?this.allMoves[indexMove]=element:this.allMoves.push(element);
+      }else {
+        this.allMoves.push(element);
+      }
+    });
+  this.cards.emit(this.allMoves); 
+}
+/*------------------------- end all crud functions on cards -------------------------- */
 
   
 }
