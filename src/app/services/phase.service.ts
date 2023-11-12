66fac69a0d024a4d55984e07185f8ee329a9426f
Rewas Safwat <62558986+rewas20@users.noreply.github.com>
2023-11-12 05:24:10 +0200

    Revert "Revert "Feature/group task apis""

--- src/app/services/phase.service.ts
+++ src/app/services/phase.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable  } from 'rxjs';
 import { TokenService } from './token.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PhaseService {
 
   private api_phases = 'http://127.0.0.1:8000/api/phases';
+
   private headers: HttpHeaders = new HttpHeaders();
 
   constructor(private http: HttpClient,private tokenService:TokenService) { 
@@ -15,6 +18,7 @@ export class PhaseService {
       'Authorization': `Bearer ${tokenService.getToken()}`
     })
   }
+
 
 
   getAllPhases() {
