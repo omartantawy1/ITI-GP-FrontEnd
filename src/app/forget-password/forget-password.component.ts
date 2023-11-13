9a328037b3c35eb0ff4b5e78e7415f49da5c34a4
Moataz Adel <wezza1933@gmail.com>
2023-11-13 12:54:56 +0200

    connected forget/reset password apis and updated the functionality of components

--- src/app/forget-password/forget-password.component.ts
+++ src/app/forget-password/forget-password.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ForgetPasswordService } from '../services/forget-password.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forget-password',
@@ -7,4 +10,50 @@ import { Component } from '@angular/core';
 })
 export class ForgetPasswordComponent {
 
+  forgetPasswordForm!: FormGroup;
+  submitted = false;
+  errormsg: any = '';
+  res: any = '';
+
+  constructor(private fb: FormBuilder, private forgetPasswordService: ForgetPasswordService, private router: Router) {}
+
+  ngOnInit(){
+    this.forgetPasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.forgetPasswordForm.valid) {
+      this.forgetPasswordService.postForgetPassword(this.forgetPasswordForm.value).subscribe(
+
+        (response: any) => {
+          this.errormsg = '';
+          this.res = '';
+          this.res = response.message;
+            console.log(response);
+        },
+        (error) => {
+          this.res = '';
+          this.errormsg = '';
+          this.errormsg = error.error.message;
+          console.error(this.errormsg, error);
+        }
+        );
+      // You can add your sign-in logic here, e.g., authenticate the user.
+      // After successful sign-in, navigate to the appropriate page.
+    } else {
+      console.log('invalid');
+    }
+  }
+
+  clearApiError(){
+    this.errormsg = '';
+  }
+
+  redirectToSignIn(){
+    this.router.navigate(['sign-in']);
+  }
 }
