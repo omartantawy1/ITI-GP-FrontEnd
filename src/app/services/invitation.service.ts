eff41de6acc870d901b21aac2895ac42d7c29267
Rewas Safwat <rewasssafwat51@gmail.com>
2023-11-15 16:03:38 +0200

    invitation

--- src/app/services/invitation.service.ts
+++ src/app/services/invitation.service.ts
@@ -7,6 +7,8 @@ import { TokenService } from './token.service';
 })
 export class InvitationService {
   private invitation_api = "http://127.0.0.1:8000/api/send-invitation";
+  private accept_api = "http://127.0.0.1:8000/api/accept-invitation";
+  private decline_api = "http://127.0.0.1:8000/api/decline-invitation";
   
   private headers: HttpHeaders = new HttpHeaders();
   constructor(private http: HttpClient,private tokenService:TokenService) { 
@@ -19,6 +21,12 @@ export class InvitationService {
   
   sendInvitaion(invitation:any) {
     return this.http.post(this.invitation_api,invitation,{headers:this.headers});
+  }
+  accept(id:number) {
+    return this.http.post(`${this.accept_api}/${id}`,{headers:this.headers});
+  }
+  decline(id:number) {
+    return this.http.post(`${this.decline}/${id}`,{headers:this.headers});
   }
 
 }
